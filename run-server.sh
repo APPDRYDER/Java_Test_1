40b3bde9ecab4f8da28edfa20f7d5aaa0eb5607d
David <davidryder@yahoo.com>
2019-06-18 18:01:01 -0700

    TestQueue1

--- run-server.sh
+++ run-server.sh
@@ -1,20 +1,30 @@
 #!/bin/bash
 #
 #
-ITERATIONS=${1:-"2"}
+DURATION_SEC=${1:-"3600"}
 WAIT_MS=${2:-"15000"}
 NODE_SEQ=`echo ${3:-"1"} | sed 's/\,/ /g'` # Expects n or n,n,... with no spaces
+JAVA_APPLICATION_NAME=${4:-"TestApp1"}
 
-APPLICATION_NAME=TestApp1
+_validateEnvironmentVars() {
+  echo "Validating environment variables for $1"
+  shift 1
+  VAR_LIST=("$@") # rebuild using all args
+  #echo $VAR_LIST
+  for i in "${VAR_LIST[@]}"; do
+     [ -z ${!i} ] && { echo "Environment variable not set: $i"; ERROR="1"; }
+  done
+  [ "$ERROR" == "1" ] && { echo "Exiting"; exit 1; }
+}
 
-# Agent config
-export APPDYNAMICS_AGENT_NODE_NAME=NODE_JT1
-export APPDYNAMICS_AGENT_TIER_NAME=TIER_JT1
+_validateEnvironmentVars "Run Server" \
+  "APPDYNAMICS_AGENT_APPLICATION_NAME" "APPDYNAMICS_AGENT_TIER_NAME" "APPDYNAMICS_AGENT_NODE_NAME" \
+  "APPDYNAMICS_APP_AGENT_JAR_FILE"
 
 export JAVA_OPTS=""
 export JAVA_OPTS=$JAVA_OPTS"-Dappdynamics.low.entropy=true "
 export JAVA_OPTS=$JAVA_OPTS"-Dallow.unsigned.sdk.extension.jars=true "
-export JAVA_OPTS=$JAVA_OPTS"-cp /Users/david.ryder/Downloads/AppD-Downloads/AppServerAgent-4.5.4.24355/ver4.5.4.24355/javaagent.jar:."
+export JAVA_OPTS=$JAVA_OPTS"-cp $APPDYNAMICS_APP_AGENT_JAR_FILE:."
 
 # Network Visibility
 #export LD_PRELOAD="/home/ddr/agent-net/lib/appd-netlib.so "
@@ -27,29 +37,28 @@ else
   exit 0
 fi
 
-SERVER_PID_FILE="/TMP/TestApp1-test-server1.PID"
-LOAD_PID_FILE="/TMP/TestApp1-load1.PID"
-
 _startServer1() {
   NAME=$1
+  ERROR_RATE=$2
+  THREADS=1
   export APPDYNAMICS_AGENT_NODE_NAME=$NAME
-  TS=`date +%Y%m%d-%H%M%S`
-  PID_FILE="/TMP/jetty-test-server1.PID"
-  echo "Starting: $APPDYNAMICS_AGENT_NODE_NAME $JAVA_OPTS"
-  nohup java $JAVA_OPTS $APPLICATION_NAME $ITERATIONS $WAIT_MS $NAME &
-
-  #echo $! > $SERVER_PID_FILE
-  #cat $SERVER_PID_FILE
+  echo "Starting: $APPDYNAMICS_AGENT_NODE_NAME $JAVA_APPLICATION_NAME $JAVA_OPTS"
+  nohup java $JAVA_OPTS $JAVA_APPLICATION_NAME $DURATION_SEC $WAIT_MS $ERROR_RATE $THREADS $NAME &
 }
+
+# Kill existing processes
+echo "Stopping existing applications instances"
+pkill -e -f "$JAVA_APPLICATION_NAME"
 
 echo "Starting: Iterations: $ITERATIONS Wait: $WAIT_MS Nodes: $NODE_SEQ"
 # Stop Previous
-#pkill -9 -f "$APPLICATION_NAME"
+#pkill -9 -f "$APPDYNAMICS_AGENT_APPLICATION_NAME"
 
 # Start N nodes
 BASE_NODE_NAME=$APPDYNAMICS_AGENT_NODE_NAME
+ERRROR_RATE="0.2"
 for NODE_ID in $NODE_SEQ; do
-  _startServer1 $BASE_NODE_NAME"_$NODE_ID"
+  _startServer1 $BASE_NODE_NAME"_$NODE_ID_" "$ERRROR_RATE"
 done
 
 
