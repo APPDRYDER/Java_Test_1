4b781aa3861d75a730897d67581b9dc217f2bcc5
David <davidryder@yahoo.com>
2019-06-21 10:16:53 -0700

    first commit

---
+++ envvars.sh
@@ -0,0 +1,7 @@
+# Base directory for AppDYnamics Java agent
+# Configure this based on directory location of the Java Agent and its version
+export APPD_JAVA_AGENT_DIR=/Users/david.ryder/Downloads/AppD-Downloads/AppServerAgent-4.5.4.24355
+
+# AppDynamics javaagent.jar file
+export APPDYNAMICS_APP_AGENT_JAR_FILE=$APPD_JAVA_AGENT_DIR/javaagent.jar
+
