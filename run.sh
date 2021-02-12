7e3b3a5c7feae4b05920d311aace2f0584ac4181
David Ryder <38921518+APPDRYDER@users.noreply.github.com>
2021-02-11 16:12:09 -0800

    Check envvars

--- run.sh
+++ run.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 #
 
-java -javaagent:$JAVA_AGENT TestApp1
-
+if [[ ! -v JAVA_AGENT ]]; then
+        echo "Running without envvar JAVA_AGENT set"
+        java TestApp1
+else
+        echo "Running with envvar JAVA_AGENT set to [$JAVA_AGENT]"
+        java -javaagent:$JAVA_AGENT TestApp1
+fi
